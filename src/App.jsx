0dff53fe3027203b0908c0332ac986486a73a210
Flames <vibe-coder@flames.com>
2025-11-08 19:43:17 +0000

    Update project prj_pgqeze4a - 2025-11-08T19:43:17.115Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import ContractBar from './components/ContractBar'
-import HowItWorks from './components/HowItWorks'
-import Story from './components/Story'
 import Waitlist from './components/Waitlist'
-import WhySolana from './components/WhySolana'
 import CTA from './components/CTA'
 
 function App() {
@@ -14,14 +10,12 @@ function App() {
       <Navbar />
       <main>
         <Hero />
-        <ContractBar />
-        <HowItWorks />
-        <Story />
-        <WhySolana />
         <Waitlist />
         <CTA />
       </main>
-      <footer className="pb-10" />
+      <footer className="pb-10 text-center text-sm text-white/50">
+        © {new Date().getFullYear()} Polytoly. All rights reserved.
+      </footer>
     </div>
   )
 }
